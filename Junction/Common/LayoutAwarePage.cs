0ef6c70db0cf6defd2576ae910a9415a5eb477b2
Adron Hall <adronhall@gmail.com>
2013-08-11 21:32:35 -0700

    Lookin at and cleaning up the code code cuz I am fiddlin.

--- Junction/Common/LayoutAwarePage.cs
+++ Junction/Common/LayoutAwarePage.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.System;
 using Windows.UI.Core;
@@ -13,92 +11,55 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Junction.Common
 {
-    /// <summary>
-    /// Typical implementation of Page that provides several important conveniences:
-    /// <list type="bullet">
-    /// <item>
-    /// <description>Application view state to visual state mapping</description>
-    /// </item>
-    /// <item>
-    /// <description>GoBack, GoForward, and GoHome event handlers</description>
-    /// </item>
-    /// <item>
-    /// <description>Mouse and keyboard shortcuts for navigation</description>
-    /// </item>
-    /// <item>
-    /// <description>State management for navigation and process lifetime management</description>
-    /// </item>
-    /// <item>
-    /// <description>A default view model</description>
-    /// </item>
-    /// </list>
-    /// </summary>
     [Windows.Foundation.Metadata.WebHostHidden]
     public class LayoutAwarePage : Page
     {
-        /// <summary>
-        /// Identifies the <see cref="DefaultViewModel"/> dependency property.
-        /// </summary>
         public static readonly DependencyProperty DefaultViewModelProperty =
             DependencyProperty.Register("DefaultViewModel", typeof(IObservableMap<String, Object>),
             typeof(LayoutAwarePage), null);
 
         private List<Control> _layoutAwareControls;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LayoutAwarePage"/> class.
-        /// </summary>
         public LayoutAwarePage()
         {
             if (Windows.ApplicationModel.DesignMode.DesignModeEnabled) return;
 
-            // Create an empty default view model
-            this.DefaultViewModel = new ObservableDictionary<String, Object>();
+            DefaultViewModel = new ObservableDictionary<String, Object>();
 
-            // When this page is part of the visual tree make two changes:
-            // 1) Map application view state to visual state for the page
-            // 2) Handle keyboard and mouse navigation requests
-            this.Loaded += (sender, e) =>
+            Loaded += (sender, e) =>
             {
-                this.StartLayoutUpdates(sender, e);
+                StartLayoutUpdates(sender, e);
 
-                // Keyboard and mouse navigation only apply when occupying the entire window
-                if (this.ActualHeight == Window.Current.Bounds.Height &&
-                    this.ActualWidth == Window.Current.Bounds.Width)
+                if (ActualHeight == Window.Current.Bounds.Height &&
+                    ActualWidth == Window.Current.Bounds.Width)
                 {
-                    // Listen to the window directly so focus isn't required
                     Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated +=
                         CoreDispatcher_AcceleratorKeyActivated;
                     Window.Current.CoreWindow.PointerPressed +=
-                        this.CoreWindow_PointerPressed;
+                        CoreWindow_PointerPressed;
                 }
             };
 
-            // Undo the same changes when the page is no longer visible
-            this.Unloaded += (sender, e) =>
+            Unloaded += (sender, e) =>
             {
-                this.StopLayoutUpdates(sender, e);
+                StopLayoutUpdates(sender, e);
                 Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated -=
                     CoreDispatcher_AcceleratorKeyActivated;
                 Window.Current.CoreWindow.PointerPressed -=
-                    this.CoreWindow_PointerPressed;
+                    CoreWindow_PointerPressed;
             };
         }
 
-        /// <summary>
-        /// An implementation of <see cref="IObservableMap&lt;String, Object&gt;"/> designed to be
-        /// used as a trivial view model.
-        /// </summary>
         protected IObservableMap<String, Object> DefaultViewModel
         {
             get
             {
-                return this.GetValue(DefaultViewModelProperty) as IObservableMap<String, Object>;
+                return GetValue(DefaultViewModelProperty) as IObservableMap<String, Object>;
             }
 
             set
             {
-                this.SetValue(DefaultViewModelProperty, value);
+                SetValue(DefaultViewModelProperty, value);
             }
         }
 
@@ -398,18 +359,14 @@ namespace Junction.Common
 
         #endregion
 
-        /// <summary>
-        /// Implementation of IObservableMap that supports reentrancy for use as a default view
-        /// model.
-        /// </summary>
         private class ObservableDictionary<K, V> : IObservableMap<K, V>
         {
             private class ObservableDictionaryChangedEventArgs : IMapChangedEventArgs<K>
             {
                 public ObservableDictionaryChangedEventArgs(CollectionChange change, K key)
                 {
-                    this.CollectionChange = change;
-                    this.Key = key;
+                    CollectionChange = change;
+                    Key = key;
                 }
 
                 public CollectionChange CollectionChange { get; private set; }
@@ -430,20 +387,20 @@ namespace Junction.Common
 
             public void Add(K key, V value)
             {
-                this._dictionary.Add(key, value);
-                this.InvokeMapChanged(CollectionChange.ItemInserted, key);
+                _dictionary.Add(key, value);
+                InvokeMapChanged(CollectionChange.ItemInserted, key);
             }
 
             public void Add(KeyValuePair<K, V> item)
             {
-                this.Add(item.Key, item.Value);
+                Add(item.Key, item.Value);
             }
 
             public bool Remove(K key)
             {
-                if (this._dictionary.Remove(key))
+                if (_dictionary.Remove(key))
                 {
-                    this.InvokeMapChanged(CollectionChange.ItemRemoved, key);
+                    InvokeMapChanged(CollectionChange.ItemRemoved, key);
                     return true;
                 }
                 return false;
@@ -452,10 +409,10 @@ namespace Junction.Common
             public bool Remove(KeyValuePair<K, V> item)
             {
                 V currentValue;
-                if (this._dictionary.TryGetValue(item.Key, out currentValue) &&
+                if (_dictionary.TryGetValue(item.Key, out currentValue) &&
                     Object.Equals(item.Value, currentValue) && this._dictionary.Remove(item.Key))
                 {
-                    this.InvokeMapChanged(CollectionChange.ItemRemoved, item.Key);
+                    InvokeMapChanged(CollectionChange.ItemRemoved, item.Key);
                     return true;
                 }
                 return false;
@@ -465,53 +422,53 @@ namespace Junction.Common
             {
                 get
                 {
-                    return this._dictionary[key];
+                    return _dictionary[key];
                 }
                 set
                 {
-                    this._dictionary[key] = value;
-                    this.InvokeMapChanged(CollectionChange.ItemChanged, key);
+                    _dictionary[key] = value;
+                    InvokeMapChanged(CollectionChange.ItemChanged, key);
                 }
             }
 
             public void Clear()
             {
-                var priorKeys = this._dictionary.Keys.ToArray();
-                this._dictionary.Clear();
+                var priorKeys = _dictionary.Keys.ToArray();
+                _dictionary.Clear();
                 foreach (var key in priorKeys)
                 {
-                    this.InvokeMapChanged(CollectionChange.ItemRemoved, key);
+                    InvokeMapChanged(CollectionChange.ItemRemoved, key);
                 }
             }
 
             public ICollection<K> Keys
             {
-                get { return this._dictionary.Keys; }
+                get { return _dictionary.Keys; }
             }
 
             public bool ContainsKey(K key)
             {
-                return this._dictionary.ContainsKey(key);
+                return _dictionary.ContainsKey(key);
             }
 
             public bool TryGetValue(K key, out V value)
             {
-                return this._dictionary.TryGetValue(key, out value);
+                return _dictionary.TryGetValue(key, out value);
             }
 
             public ICollection<V> Values
             {
-                get { return this._dictionary.Values; }
+                get { return _dictionary.Values; }
             }
 
             public bool Contains(KeyValuePair<K, V> item)
             {
-                return this._dictionary.Contains(item);
+                return _dictionary.Contains(item);
             }
 
             public int Count
             {
-                get { return this._dictionary.Count; }
+                get { return _dictionary.Count; }
             }
 
             public bool IsReadOnly
@@ -521,18 +478,18 @@ namespace Junction.Common
 
             public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
             {
-                return this._dictionary.GetEnumerator();
+                return _dictionary.GetEnumerator();
             }
 
             System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
             {
-                return this._dictionary.GetEnumerator();
+                return _dictionary.GetEnumerator();
             }
 
             public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
             {
-                int arraySize = array.Length;
-                foreach (var pair in this._dictionary)
+                var arraySize = array.Length;
+                foreach (var pair in _dictionary)
                 {
                     if (arrayIndex >= arraySize) break;
                     array[arrayIndex++] = pair;
