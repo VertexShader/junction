fb86868539e4fe48bc279e10d214b672d8502767
Adron Hall <adronhall@gmail.com>
2013-08-18 20:26:16 -0700

    Added the feed reader objects for pulling in the Basho Blog data feed.

---
+++ Junction/FeedData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Junction
+{
+    public class FeedData
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+
+        private List<FeedItem> _Items = new List<FeedItem>();
+        public List<FeedItem> Items
+        {
+            get
+            {
+                return this._Items;
+            }
+        }
+    }
+}
---
+++ Junction/FeedDataSource.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+
+namespace Junction
+{
+    public class FeedDataSource
+    {
+        private ObservableCollection<FeedData> _Feeds = new ObservableCollection<FeedData>();
+        public ObservableCollection<FeedData> Feeds
+        {
+            get
+            {
+                return this._Feeds;
+            }
+        }
+
+        public async Task GetFeedsAsync()
+        {
+            Task<FeedData> feed9 =
+                GetFeedAsync("http://feeds.feedburner.com/LooselyCoupledHumanCodeFactory");
+            Task<FeedData> feed10 =
+                GetFeedAsync("http://basho.com/blog/ato/");
+
+            this.Feeds.Add(await feed9);
+            this.Feeds.Add(await feed10);
+        }
+
+        private async Task<FeedData> GetFeedAsync(string feedUriString)
+        {
+            var client = new SyndicationClient();
+            var feedUri = new Uri(feedUriString);
+
+            try
+            {
+                SyndicationFeed feed = await client.RetrieveFeedAsync(feedUri);
+
+                // This code is executed after RetrieveFeedAsync returns the SyndicationFeed.
+                // Process it and copy the data we want into our FeedData and FeedItem classes.
+                var feedData = new FeedData();
+
+                feedData.Title = feed.Title.Text;
+                if (feed.Subtitle != null && feed.Subtitle.Text != null)
+                {
+                    feedData.Description = feed.Subtitle.Text;
+                }
+                // Use the date of the latest post as the last updated date.
+                feedData.PubDate = feed.Items[0].PublishedDate.DateTime;
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    FeedItem feedItem = new FeedItem();
+                    feedItem.Title = item.Title.Text;
+                    feedItem.PubDate = item.PublishedDate.DateTime;
+                    feedItem.Author = item.Authors[0].Name.ToString();
+                    // Handle the differences between RSS and Atom feeds.
+                    if (feed.SourceFormat == SyndicationFormat.Atom10)
+                    {
+                        feedItem.Content = item.Content.Text;
+                        feedItem.Link = new Uri("http://windowsteamblog.com" + item.Id);
+                    }
+                    else if (feed.SourceFormat == SyndicationFormat.Rss20)
+                    {
+                        feedItem.Content = item.Summary.Text;
+                        feedItem.Link = item.Links[0].Uri;
+                    }
+                    feedData.Items.Add(feedItem);
+                }
+                return feedData;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
