7b2b0fecec68534138797a9ec5a4ae026a51fddd
Adron Hall <adronhall@gmail.com>
2013-08-19 23:13:42 -0700

    Changed App1 name to Junction.

--- Junction/ItemDetailPage.xaml.cs
+++ Junction/ItemDetailPage.xaml.cs
@@ -1,21 +1,11 @@
-﻿using System;
+﻿// The Item Detail Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234232
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-// The Item Detail Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234232
 using Junction.Common;
 using Junction.DataModel;
 
-namespace App1
+namespace Junction
 {
     /// <summary>
     /// A page that displays details for a single item within a group while allowing gestures to
